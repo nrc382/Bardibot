acb10026ce90ec426a5bc27be0de674ffac1e447
nrc382 <gglnrc@me.com>
2023-05-21 09:52:31 +0200

    Pubblicazione di albero_query

---
+++ Utils/albero_query.js
@@ -0,0 +1,39 @@
+// Non mi convince molto, questo albero_query…
+// Dovrebbe semplificare la vita quando si va a scrivere il codice… Vedrem!
+
+// Agiornamento dopo 2 giorni. Ora nel message_model c'è genera_percorso_callback e… UAU!
+
+module.exports = {
+    utente: {
+        stmp: "UTENTE",
+        menu: {
+            stmp: "MENU",
+            
+        },
+        mostra_id: {
+            stmp: "MOSTRA_ID",
+        },
+        registrazione: {
+            stmp: "REG",
+            sviluppo: {
+                stmp: "DEV",
+            
+            },
+            esempio: {
+                stmp: "INFO",
+            
+            },
+            conferma: {
+                stmp: "CONF_REG",
+            
+            }
+        }
+    },
+    
+    chiudi: {
+        stmp: "CHIUDI",
+        puff: {
+            stmp: "PUFF"
+        }
+    }
+}
